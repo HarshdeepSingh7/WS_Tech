38b2bc519a156ab7bdf82026bbab4e938b160e7c
Harshdeep Singh <deeph5242@gmail.com>
2025-11-09 15:12:46 +0530

    Initial commit of my website

---
+++ main.js
@@ -0,0 +1,19 @@
+//change navbar style o scroll//
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 100)
+     })
+
+
+
+//show hide faq answer//
+// 
+
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        
+    })
+})
